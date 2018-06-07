842395a88f034ab3b5b8a665bc72df6aa76d574b
liweihua <weihua0915@163.com>
2018-06-07 15:45:43 +0800

    update usbreader

--- Framework/Device/USBCOMReader.py
+++ Framework/Device/USBCOMReader.py
@@ -1,5 +1,6 @@
-from ctypes import *
 from ArgonDevice import *
+from ArgonLog import *
+from ctypes import *
 import win32api
 
 
@@ -7,8 +8,10 @@ class USBCOMReader(ArgonDevice):
     def __init__(self):
         self.dll = ""
         self.hDev = ""
+        self.ATR = ""
 
-    def openPort(self, port):
+    @recordopen
+    def openport(self, port):
         if port[0:3] == "USB":
             param1 = c_int(1)
             param2 = c_char(b"0")
@@ -18,94 +21,76 @@ class USBCOMReader(ArgonDevice):
             param2 = c_char(b"E")
             nad = c_char(0x12)
         else:
-            print("读卡器不支持")
-            return False
+            raise DeviceError('No Device ' + port)
         try:
             self.dll = windll.LoadLibrary("wdcrwv.dll")
-            str = port.encode("utf8")
-            name = c_char_p(str)
+            name = c_char_p(port.encode("utf8"))
             self.hDev = self.dll.CT_open(name, param1, param2)
             if self.hDev == -1:
-                print("读卡器连接失败")
-                return False
+                raise DeviceError('Open ' + port + 'Failed')
             else:
-                print("读卡器连接成功")
                 self.dll.ICC_set_NAD(self.hDev,nad)
-                return True
+                return label_pass
         except Exception:
-            print("读卡器连接失败")
-            return False
+            raise DeviceError('Open ' + port + 'Failed')
 
+    @recordreset
     def reset(self):
         try:
             lenresp = create_string_buffer(1)
             resp = create_string_buffer(255)
             rtn = self.dll.ICC_reset(self.hDev,lenresp, resp)
-            rtn = hex(rtn)[2:].upper()
-            if rtn == "9000":
-                len1 = int(lenresp.raw.hex(),16)
-                RtnData = resp.raw.hex()[0:len1 * 2].upper()
-
-                print("ATR:" + RtnData)
-                return RtnData,True
+            # 返回响应码9000
+            if rtn == 36864:
+                self.ATR = resp.raw.hex()[0:int(lenresp.raw.hex(),16) * 2].upper()
+                return self.ATR,label_pass
             else:
-                print("复位失败")
-                return "",False
+                raise DeviceError('Rest Device Failed')
         except Exception:
-            print("复位失败")
-            return "",False
+            raise DeviceError('Rest Device Failed')
 
-    def senDisplay(self, APDUdata, *SW,expectData = ""):
-        state = True
+    @recordapdu
+    def sendisplay(self, apdudata, msg='', **kwargs):
+        status = label_pass
         try:
-            comm = bytes.fromhex(APDUdata)
+            comm = bytes.fromhex(apdudata)
             strlen = len(comm)
             lenresp = create_string_buffer(1)
             resp = create_string_buffer(255)
             rtn = self.dll.ICC_tsi_api(self.hDev, strlen, comm, lenresp, resp)
-            rtn = hex(rtn)[2:].upper()
-            len1= int(lenresp.raw.hex(),16)
-            RtnData = resp.raw.hex()[0:len1*2].upper()
-            ArgonDevice.showAPDU(APDUdata,RtnData,expectData,rtn,SW)
+            rtn_sw = hex(rtn)[2:].upper()
+            rtn_data = resp.raw.hex()[0:int(lenresp.raw.hex(),16)*2].upper()
         except Exception:
-            print("指令发送异常")
-            state = False
-            return "", "", state
+            raise DeviceError('Send APDU Failed')
         else:
-            try:
-                ArgonDevice.checkRespData(RtnData, expectData)
-            except DataError as e:
-                print(e.detail)
-                # self.errorResult.append(e.detail)
-                state = False
-            finally:
-                try:
-                    ArgonDevice.checkSW(rtn, SW)
-                except SWError as e:
-                    print(e.detail)
-                    # self.errorResult.append(e.detail)
-                    state = False
-            return RtnData, rtn, state
+            if 'expect_data' in kwargs.keys():
+                if not self.checkrespdata(rtn_data, kwargs['expect_data']):
+                    status = label_fail
 
-    def closePort(self):
+            if 'sw' in kwargs.keys():
+                if not self.checksw(rtn_sw, kwargs['sw']):
+                    status = label_fail
+
+            return rtn_data, rtn_sw, status
+
+    @recordclose
+    def closeport(self):
         try:
             self.dll.CT_close(self.hDev)
             win32api.FreeLibrary(self.dll._handle)
-            return True
+            return label_pass
         except Exception:
-            print("读卡器断开失败")
-            return False
+            raise DeviceError('Close Device Failed')
 
 
 if __name__ == '__main__':
 
-    #for num in range(0,1):
     jts = USBCOMReader()
-    jts.openPort("USB1")
+    jts.openport("USB1")
     jts.reset()
-    jts.senDisplay("00A4040007A0000000041010")
-    jts.senDisplay("00B2021400")
-    jts.closePort()
+    jts.sendisplay("00A4040007A0000000041010")
+    jts.sendisplay("00B2021400")
+    jts.closeport()
 
     #resp1, rtn1, state = jts.senDisplay("00A4040007A0000000041010", "9000", "6A88",expectData="6F3D8407A0000000041010A532500A4D6173746572636172648701019F1101019F120A4D617374657263617264BF0C0F9F4D020B0A9F6E0701560000303000")
     """
